bc7235afaed15f9bdfd3321cba2a0289987bc9a9
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2023-06-20 18:33:38 -0300

    Delete assets/preload/data/rolebreaker directory

--- assets/preload/data/rolebreaker/script8.lua
+++
@@ -1,5 +0,0 @@
-function onCreate()
-	setPropertyFromClass('GameOverSubstate', 'deathSoundName', '')
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', '')
-	setPropertyFromClass('GameOverSubstate', 'endSoundName', '')
-end

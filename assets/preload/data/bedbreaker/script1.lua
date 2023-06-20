52790a04b5e0b6de9bbb73db1eafe86682508bfe
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2023-06-20 14:18:19 -0300

    Add files via upload

--- assets/preload/data/bedbreaker/script1.lua
+++ assets/preload/data/bedbreaker/script1.lua
@@ -1,57 +1,20 @@
-local creditShown = false
-
-function onStartCountdown()
-	if not creditShown then
-		setPropertyFromClass('PauseSubState', 'songName', 'bedpauser')
-		
-		makeAnimatedLuaSprite('creditSchmoovin','songCredits/creditThingAnimation', -1400, -300)
-		scaleObject('creditSchmoovin', 1, 1)
-		addLuaSprite('creditSchmoovin',true);
-		setScrollFactor('creditSchmoovin',0,0);
-		setObjectCamera('creditSchmoovin','hud');
-		addAnimationByPrefix('creditSchmoovin','schmoove','thing')
-		setProperty('creditSchmoovin.angle', -30)
-		
-		makeLuaSprite('creditSlide','songCredits/slide'..songName, 0, 990)
-		scaleObject('creditSlide', 1.1, 1.1)
-		addLuaSprite('creditSlide',true);
-		setScrollFactor('creditSlide',0,0);
-		setObjectCamera('creditSlide','hud');
-		
-		makeLuaSprite('creditSpikes','songCredits/spikeys', 1900, 0)
-		scaleObject('creditSpikes', 1.2, 1.2)
-		addLuaSprite('creditSpikes',true);
-		setScrollFactor('creditSpikes',0,0);
-		setObjectCamera('creditSpikes','hud');
-		
-		makeLuaSprite('creditCircle','songCredits/circle'..songName, 1800, 200)
-		scaleObject('creditCircle', 0.9, 0.9)
-		addLuaSprite('creditCircle',true);
-		setScrollFactor('creditCircle',0,0);
-		setObjectCamera('creditCircle','hud');
-		
-		doTweenX("credSchmooveTween", "creditSchmoovin", -400, 1.0, "sineOut")
-		doTweenY("credSlideTween", "creditSlide", 490, 1.0, "sineOut")
-		doTweenX("credSpikeTween", "creditSpikes", 900, 1.5, "sineOut")
-		doTweenX("credCircTween", "creditCircle", 800, 1.0, "sineOut")
-		
-		creditShown = true
-	end
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', '')
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', '')
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'boowomp')
 end
 
-function onSongStart()
-	doTweenX("credSchmooveTween", "creditSchmoovin", -1400, 1, "sineIn")
-	doTweenY("credSlideTween", "creditSlide", -400, 0.75, "sineIn")
-	doTweenX("credSpikeTween", "creditSpikes", 1900, 1.5, "sineIn")
-	doTweenX("credCircTween", "creditCircle", 1750, 1, "sineIn")
-	runTimer('credTween', 2)
+function onGameOverStart()
+	setProperty('boyfriend.alpha', 0)
+	
+	makeLuaSprite('lois','heyLois', 550, -875)
+	addLuaSprite('lois',true);
+	setScrollFactor('lois',0,0);
+	setObjectCamera('lois','hud');
+	doTweenY("loisJumpscare", "lois", 175, 2.0, "bounceOut")
 end
 
-function onTimerCompleted(t)
-	if t == 'credTween' then
-		removeLuaSprite('creditSchmoovin')
-		removeLuaSprite('creditSlide')
-		removeLuaSprite('creditSpikes')
-		removeLuaSprite('creditCircle')
-	end
+function onGameOverConfirm()
+	playSound('boowomp', 1)
+	doTweenY("loisDies", "lois.scale", 0, 0.4, "quartIn")
 end

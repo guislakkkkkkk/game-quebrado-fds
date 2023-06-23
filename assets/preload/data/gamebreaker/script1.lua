a7e48f66b7c290f14a25c06ecd4c0052ce5f09d6
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2023-06-22 21:29:25 -0300

    Add files via upload

--- assets/preload/data/gamebreaker/script1.lua
+++ assets/preload/data/gamebreaker/script1.lua
@@ -1,33 +1,27 @@
 local beatLength=0
 local stepLength=0
 
-local charName='DXcomic'
-local charDirectory='characters/dxComic'
+local charName='picomic'
+local charDirectory='characters/picoComic'
 local charScale=1
 local offsetScalesWithSize=true
 local correspondingNoteType='ExtraCharNote1'
 local singLength=4
 
-local charPos={-875, 150}
+local charPos={1450, 150}
 local prefixes={
-		[1]='dxcomicleft', --[[left]]
-		[2]='dxcomicdown', --[[down]]
-		[3]='dxcomicup', --[[up]]
-		[4]='dxcomicright', --[[right]]
-		[5]='dxcomicidle', --[[idle]]
-		[6]='dxcomicP1', --[[1]]
-		[7]='dxcomicP2', --[[2]]
-		[8]='dxcomicP3', --[[3]]
+		[1]='pico comic left', --[[left]]
+		[2]='pico comic down', --[[down]]
+		[3]='pico comic up', --[[up]]
+		[4]='pico comic right', --[[right]]
+		[5]='pico comic idle', --[[idle]]
 	}
 local charOffsets={
 		[1]={0, 90}, --[[left]]
 		[2]={0, 90}, --[[down]]
 		[3]={0, 90}, --[[up]]
-		[4]={1, 90}, --[[right]]
-		[5]={1, 90}, --[[idle]]
-		[6]={0, 0}, --[[1]]
-		[7]={0, 0}, --[[2]]
-		[8]={0, 0}, --[[3]]
+		[4]={0, 90}, --[[right]]
+		[5]={0, 90}, --[[idle]]
 	}
 
 function mathStuffs()
@@ -57,31 +51,28 @@ function onCreatePost()
 		addAnimationByPrefix(charName, 'singUP', prefixes[3], 24, false)
 		addAnimationByPrefix(charName, 'singRIGHT', prefixes[4], 24, false)
 		addAnimationByPrefix(charName, 'idle', prefixes[5], 24, false)
-		addAnimationByPrefix(charName, '1', prefixes[6], 24, false)
-		addAnimationByPrefix(charName, '2', prefixes[7], 24, false)
-		addAnimationByPrefix(charName, '3', prefixes[8], 24, false)
 
 	advAnim(charName, 'idle' , true, charOffsets[5])
 	setObjectOrder(charName, 0)
 	setScrollFactor(charName,0,0);
 	setObjectCamera(charName,'hud');
 	scaleObject(charName, 1.3, 1.3)
-	setProperty('DXcomic.visible', false)
+	setProperty('picomic.visible', false)
 end
 
---function goodNoteHit(id,dir,note,sus)
-	--if note==correspondingNoteType then
-		--advAnim(charName, singAnims[dir+1], true, charOffsets[dir+1])
-		--runTimer(charName..'-holdTimer', stepLength*singLength, 1)
-	--end
---end
-
-function opponentNoteHit(id,dir,note,sus)
+function goodNoteHit(id,dir,note,sus)
 	--if note==correspondingNoteType then
 		advAnim(charName, singAnims[dir+1], true, charOffsets[dir+1])
 		runTimer(charName..'-holdTimer', stepLength*singLength, 1)
 	--end
 end
+
+--function opponentNoteHit(id,dir,note,sus)
+	--if note==correspondingNoteType then
+		--advAnim(charName, singAnims[dir+1], true, charOffsets[dir+1])
+		--runTimer(charName..'-holdTimer', stepLength*singLength, 1)
+	--end
+--end
 
 function onTimerCompleted(tag,loops,loopsLeft)
 	if tag==charName..'-holdTimer' then
@@ -97,12 +88,12 @@ end
 
 function onStepHit()
 	if songName == 'Gamebreaker' then
-		if curStep == 1732 or curStep == 1912 then
-			setProperty('DXcomic.visible', true)
-			doTweenX("DXcomictweeb", charName, -75, 0.5, "circOut")
+		if curStep == 1796 or curStep == 2040 then
+			setProperty('picomic.visible', true)
+			doTweenX("picomictweeb", charName, 650, 0.5, "circOut")
 		end
-		if curStep == 1856 or curStep == 1920 then
-			doTweenX("DXcomictweeb", charName, -875, 0.5, "circInOut")
+		if curStep == 1856 or curStep == 2048 then
+			doTweenX("picomictweeb", charName, 1450, 0.5, "circInOut")
 		end
 	end
 end
